6d281d16a2fdf68dbfac13bac3393137d673b112
a14samuelsa <ariasouto@gmail.com>
2018-05-16 19:38:40 +0200

    api commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfig('client_secret_398363053644-b38trq79ji0be2lb6m2d0b24kqhcq2js.apps.googleusercontent.com.json');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive = new Google_Service_Drive($client);
+  $files = $drive->files->listFiles(array())->getFiles();
+  echo json_encode($files);
+} else {
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauthsamuel/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
